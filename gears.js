d88d7664829254ef3b0959f2a6899550b9372e35
Aneel Marshall <aneel.marshall@outlook.com>
2018-10-10 20:39:15 +0100

    add initial gears formulas

---
+++ gears.js
@@ -0,0 +1,19 @@
+
+let chainring = 52 // number of teeth
+
+let cog = 11
+
+let ratio = chainring / cog 
+console.log(ratio) 
+// 4.72727272...
+// each rotation of pedals causes wheels to travel
+// around almost five times
+
+let chainring = 30
+
+let cog = 27
+
+let ratio = chainring / cog 
+console.log(ratio) // 1.1111111111
+// easier gear, one rotation pedals moves wheels
+// a bit more than once
